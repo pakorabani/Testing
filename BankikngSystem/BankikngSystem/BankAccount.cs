39a5170455bc1f5e56f93083e98e91372ce79c84
pakorabani <n.mihajlov2019@pmg-pernik.com>
2023-02-16 10:03:06 +0200

    Testche

---
+++ BankikngSystem/BankikngSystem/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankikngSystem
+{
+    public class BankAccount
+    {
+
+        public BankAccount(int id, decimal balance = 0)
+        {
+            this.Id = id;
+            this.Balance = balance;
+        }
+        public int Id { get; set; } 
+        public decimal Balance { get; set; }
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new InvalidOperationException("Negative amount");
+            }
+            
+            this.Balance += amount;
+        }
+    }
+}
---
+++ BankikngSystem/BankingSystem.Test/BankAccountTests.cs
@@ -0,0 +1,42 @@
+﻿using BankikngSystem;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem.Test
+{
+    [TestFixture]
+    public class BankAccountTests
+    {
+        [Test]
+        public void DepositShouldIncreaseBalance()
+        {
+            {
+
+                //Arrange
+                BankAccount bankAccount = new BankAccount(123);
+                decimal depositAmount = 100;
+                //Act
+                bankAccount.Deposit(depositAmount);
+                //Assert
+                Assert.AreEqual(depositAmount, bankAccount.Balance);
+            }
+        }
+
+        [Test]
+        public void AccountInicializeWhithPositiveValue()
+        {
+            {
+                //Arrange
+
+                //Act
+                BankAccount bankAccount = new BankAccount(123, 2000m);
+                //Assert
+                Assert.AreEqual(2000m, bankAccount.Balance);
+            }
+        }
+    }
+}
